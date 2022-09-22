0378c367e797c9bf0eec5f63084e91d991fffcf6
Liu Mengyang <liumengyang@Lius-MacBook-Pro.local>
2022-09-22 12:30:19 +0800

    stage with bugs

---
+++ examples/ocr.rs
@@ -0,0 +1,83 @@
+extern crate mmdeploy;
+extern crate opencv;
+
+use std::env;
+
+use opencv::{
+    core::{Scalar, Point},
+    types::{VectorOfi32, VectorOfPoint},
+    imgcodecs::{imread, imwrite, IMREAD_COLOR},
+    imgproc::{polylines},
+};
+
+pub use mmdeploy::text_detector::{
+    text_detector_create_by_path,
+    text_detector_apply,
+    text_detector_release_result,
+    text_detector_release,
+};
+
+pub use mmdeploy::text_recognizer::{
+    text_recognizer_create_by_path,
+    text_recognizer_apply_bbox,
+    text_recognizer_release_result,
+    text_recognizer_release,
+};
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let device_name = &args[1];
+    let det_model_path = &args[2];
+    let reg_model_path = &args[3];
+    let image_path = &args[4];
+
+    // opencv
+    let mut img = imread(image_path, IMREAD_COLOR).unwrap();
+
+    let text_detector = text_detector_create_by_path(det_model_path, device_name, 0).unwrap();
+    let text_recognizer = text_recognizer_create_by_path(reg_model_path, device_name, 0).unwrap();
+
+    println!("start");
+    let text_det_result = text_detector_apply(text_detector, &img, 1).unwrap();
+
+    println!("here");
+    let bbox_count = text_det_result.get_result_count(0);
+
+    println!("bbox_count={}", bbox_count);
+
+    let texts = text_recognizer_apply_bbox(text_recognizer, &img, 1, text_det_result.results, bbox_count).unwrap();
+
+    for i in 0..bbox_count {
+        let text = texts.get_result(0, i as isize);
+        // let mask = det_result.get_result_mask(i)
+        
+        println!("box{}: {}", i, text.get_text());
+
+        let mut poly_points = VectorOfPoint::new();
+
+        let result = text_det_result.get_result(0, i as isize);
+
+        for j in 0..4 {
+            let pt = result.get_bbox(j as isize);
+            format!("x: {}, y: {}, ", pt.x, pt.y);
+            poly_points.push(Point{x: pt.x as i32, y: pt.y as i32});
+            println!();
+        }
+        
+        let color: Scalar = Scalar::new(0.0, 255.0, 0.0, 0.0);
+
+        polylines(&mut img, &poly_points, true, color, 1, 8, 0).unwrap();
+    }
+
+    let params = VectorOfi32::new();
+
+    let succcess = imwrite("output_ocr.png", &img, &params).unwrap();
+
+    text_detector_release_result(text_det_result, 1);
+    text_detector_release(text_detector);
+    text_recognizer_release_result(texts, 1);
+    text_recognizer_release(text_recognizer);
+
+}
---
+++ src/text_recognizer.rs
@@ -0,0 +1,96 @@
+use std::os::raw::c_char;
+use std::ffi::{CString, CStr};
+use std::str::Utf8Error;
+
+use sys::*;
+use opencv::prelude::*;
+
+pub struct Text {
+    pub text: *mut c_char,
+    pub score: *mut f32,
+    pub length: i32,
+}
+
+impl Text {
+    pub fn from(recognition: mmdeploy_text_recognition_t) -> Text {
+        Text {
+            text: recognition.text,
+            score: recognition.score,
+            length: recognition.length,
+        }
+    }
+    pub fn get_text(&self) -> String {
+        unsafe {
+            let text_slice: String = String::from(CString::from(CStr::from_ptr(self.text)).to_str().unwrap());
+            text_slice
+        }
+    }
+}
+
+pub struct RecResult {
+    pub results: *mut *mut mmdeploy_text_recognition_t,
+}
+
+impl RecResult {
+    pub fn new() -> RecResult {
+        RecResult {
+            results: Box::into_raw(Box::new(mmdeploy_text_recognition_t::new())) as *mut *mut mmdeploy_text_recognition_t,
+        }
+    }
+
+    pub fn get_result(&self, image_index:isize, text_index: isize) -> Text {
+        unsafe{
+            Text::from(*(*self.results.offset(image_index)).offset(text_index))
+        }
+    }
+}
+
+pub fn text_recognizer_create_by_path(model_path: &str, device_name: &str, device_id: i32) -> Result<mmdeploy_text_recognizer_t, mmdeploy_status_t> {
+    unsafe{
+        let mut recognizer: mmdeploy_text_recognizer_t = &mut mmdeploy_text_recognizer::new();
+
+        let model_path_cstr = CString::new(&model_path[..]).unwrap();
+        let model_path_i8: *const c_char = model_path_cstr.as_ptr() as *const c_char;
+
+        let device_name_cstr = CString::new(&device_name[..]).unwrap();
+        let device_name_i8: *const c_char = device_name_cstr.as_ptr() as *const c_char;
+
+        let status: mmdeploy_status_t = mmdeploy_text_recognizer_create_by_path(model_path_i8, device_name_i8, device_id, &mut recognizer).try_into().unwrap();
+
+        if status != mmdeploy_status_t_MMDEPLOY_SUCCESS {
+            return Err(status);
+        }
+        Ok(recognizer)
+    }
+}
+
+
+pub fn text_recognizer_apply_bbox(text_recognizer: mmdeploy_text_recognizer_t, img: &Mat, mat_count: i32, bboxes: *mut *mut mmdeploy_text_detection_t, bbox_count: i32) -> Result<RecResult, mmdeploy_status_t> {
+    unsafe {
+        let mat: *const mmdeploy_mat_t = &mmdeploy_mat_t {
+            data: img.data() as *mut u8,
+            height: img.rows(),
+            width: img.cols(),
+            channel: 3,
+            format: mmdeploy_pixel_format_t_MMDEPLOY_PIXEL_FORMAT_BGR,
+            type_: mmdeploy_data_type_t_MMDEPLOY_DATA_TYPE_UINT8,
+        };
+
+        let mut recResult = RecResult::new();
+
+        let status: mmdeploy_status_t = mmdeploy_text_recognizer_apply_bbox(text_recognizer, mat, mat_count, *bboxes as *const mmdeploy_text_detection_t, &bbox_count, recResult.results).try_into().unwrap();
+
+        if status != mmdeploy_status_t_MMDEPLOY_SUCCESS {
+            return Err(status);
+        }
+        Ok(recResult)
+    }
+}
+
+pub fn text_recognizer_release_result(rec_result: RecResult, image_num: i32) {
+    unsafe { mmdeploy_text_recognizer_release_result(*rec_result.results, image_num); }
+}
+
+pub fn text_recognizer_release(text_recognizer: mmdeploy_text_recognizer_t) {
+    unsafe { mmdeploy_text_recognizer_destroy(text_recognizer); }
+}
